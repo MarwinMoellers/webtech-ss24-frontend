cc171440aa0b5aa2bd0bae2e298aa798aa06aa15
marwin <moellers@htw-berlin.de>
2024-04-30 16:00:51 +0200

    v-for Pizza List

---
+++ src/model.ts
@@ -0,0 +1,4 @@
+export type Pizza = {
+    name: string,
+    price: number
+}
--- src/views/PizzaOverview.vue
+++ src/views/PizzaOverview.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+import type { Pizza } from '@/model';
 
+const pizzen: Pizza[] = [
+  {name: 'Margarita', price: 10},
+  {name: 'Salami', price: 12},
+  {name: 'Tonno', price: 14},
+]
 </script>
 
 <template>
-Hallo Welt
+  <h1>Pizza Übersicht</h1>
+  <div v-for="pizza in pizzen" :key="pizza.name">
+    <div>Pizza: {{pizza.name}}</div>
+    <div>Preis: {{pizza.price}}€</div>
+  </div>
+
 </template>
 
 <style scoped>
